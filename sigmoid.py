de26c4bb72124105bcb49bb6032acb1185ce7fb1
Jonathan Yeh <johnnyyeh0723@gmail.com>
2021-10-05 17:41:50 +0800

    Update sigmoid.py

--- sigmoid.py
+++ sigmoid.py
@@ -10,3 +10,9 @@ if __name__ == "__main__":
     print(np.exp(data))
     print(sum(np.exp(data)))
     print(softmax(data))
+    
+### Expect output
+[ 148.4131591   403.42879349 1096.63315843]
+1648.4751110237703
+[0.09003057 0.24472847 0.66524096]
+###
